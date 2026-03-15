be169c365887602d401ca7f9b8ba54b9b6e62e06
HIEU DOAN <49260251+hieudoanm@users.noreply.github.com>
2026-03-15 21:45:14 +0700

    update prettier

--- packages/app/src/components/Divider.tsx
+++ packages/app/src/components/Divider.tsx
@@ -1,6 +1,6 @@
 import { FC } from 'react';
 
 export const Divider: FC<{ className?: string }> = ({ className = '' }) => {
-	const baseClasses = 'w-full border-t border-neutral-200';
-	return <div className={`${baseClasses} ${className}`} />;
+  const baseClasses = 'w-full border-t border-neutral-200';
+  return <div className={`${baseClasses} ${className}`} />;
 };
